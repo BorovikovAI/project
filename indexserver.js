0b29cd39b0fa5bcec443eaf169a51132e7b8ac61
BorovikovAI <67784802+BorovikovAI@users.noreply.github.com>
2020-07-10 14:26:41 +0300

    Add files via upload

---
+++ indexserver.js
@@ -0,0 +1,14 @@
+//console.log('white lives matter');
+require('./app/wtf1');
+require('./app/wtferr');
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (request, response) => {
+    response.send('Hello from Express!')
+})
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }    console.log(`server is listening on ${port}`)
+})
